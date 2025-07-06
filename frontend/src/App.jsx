129d004b4b89595ec17421fca9575f350fb44bca
Nantha001 <pulsernantha123@gmail.com>
2025-07-06 17:07:20 +0530

    v38yr913

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,9 +3,13 @@ import InputForm from './components/InputForm';
 import DisplayCard from './components/DisplayCard';
 import "./index.css"
 
+
+
+
 function App() {
   const [businessData, setBusinessData] = useState(null);
   const [formValues, setFormValues] = useState(null);
+  const [loader, setLoader] = useState(null);
 
   const handleFormSubmit = async (form) => {
     setFormValues(form);
@@ -15,7 +19,12 @@ function App() {
       body: JSON.stringify(form)
     });
     const data = await res.json();
-    setBusinessData(data);
+    if(res.ok){
+      setLoader(false);
+      setBusinessData(data);
+    }else{
+      setLoader(true)
+    }
   };
 
   const handleRegenerate = async () => {
@@ -30,6 +39,16 @@ function App() {
     <div className="max-w-md mx-auto mt-10">
       <h1 className="text-2xl font-bold text-center mb-6 heading">Mini Business Dashboard</h1>
       <InputForm onSubmit={handleFormSubmit} />
+  {loader && (
+  <div className="flex justify-center mt-4">
+    <div className="w-12 h-12 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
+  </div>
+)}
+
+
+
+
+      
       <DisplayCard data={businessData} onRegenerate={handleRegenerate} />
       
     </div>
